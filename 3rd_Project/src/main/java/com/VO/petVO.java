5d0fa6c7715d94cdaa3060657a546e3ae0534042
CGI <CGI@DESKTOP-DJVT6V1.mshome.net>
2021-07-25 15:44:58 +0900

    hello.jsp 삭제 / JoinCon 추가 / petVO 수정

--- 3rd_Project/src/main/java/com/VO/petVO.java
+++ 3rd_Project/src/main/java/com/VO/petVO.java
@@ -1,42 +1,79 @@
 package com.VO;
 
 public class petVO {
-
-	String nick;
-	String profile;
-	String introduce;
-	String species;
 	
-	public petVO(String nick, String profile, String introduce, String species) {
+	String pet_num;
+	String pet_nick;
+	String pet_profile;
+	String pet_introduce;
+	String pet_gen;
+	String species;
+	String email;
+	
+	public petVO(String pet_num, String pet_nick, String pet_profile, String pet_introduce, String pet_gen,
+			String species, String email) {
 		super();
-		this.nick = nick;
-		this.profile = profile;
-		this.introduce = introduce;
+		this.pet_num = pet_num;
+		this.pet_nick = pet_nick;
+		this.pet_profile = pet_profile;
+		this.pet_introduce = pet_introduce;
+		this.pet_gen = pet_gen;
 		this.species = species;
+		this.email = email;
 	}
 
-	public String getNick() {
-		return nick;
+	
+	public petVO(String email, String pet_num, String pet_nick, String pet_introduce) {
+		this.email = email;
+		this.pet_num = pet_num;
+		this.pet_nick = pet_nick;
+		this.pet_introduce = pet_introduce;
+	}
+	
+	public petVO(String email,  String pet_nick, String pet_introduce) {
+		this.email = email;
+		this.pet_nick = pet_nick;
+		this.pet_introduce = pet_introduce;
 	}
 
-	public void setNick(String nick) {
-		this.nick = nick;
+	public String getPet_num() {
+		return pet_num;
 	}
 
-	public String getProfile() {
-		return profile;
+	public void setPet_num(String pet_num) {
+		this.pet_num = pet_num;
 	}
 
-	public void setProfile(String profile) {
-		this.profile = profile;
+	public String getPet_nick() {
+		return pet_nick;
 	}
 
-	public String getIntroduce() {
-		return introduce;
+	public void setPet_nick(String pet_nick) {
+		this.pet_nick = pet_nick;
 	}
 
-	public void setIntroduce(String introduce) {
-		this.introduce = introduce;
+	public String getPet_profile() {
+		return pet_profile;
+	}
+
+	public void setPet_profile(String pet_profile) {
+		this.pet_profile = pet_profile;
+	}
+
+	public String getPet_introduce() {
+		return pet_introduce;
+	}
+
+	public void setPet_introduce(String pet_introduce) {
+		this.pet_introduce = pet_introduce;
+	}
+
+	public String getPet_gen() {
+		return pet_gen;
+	}
+
+	public void setPet_gen(String pet_gen) {
+		this.pet_gen = pet_gen;
 	}
 
 	public String getSpecies() {
@@ -46,4 +83,15 @@ public class petVO {
 	public void setSpecies(String species) {
 		this.species = species;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
 }
---
+++ 3rd_Project/src/main/java/com/controller/JoinCon.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/JoinCon")
+public class JoinCon extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	
+	
+	}
+
+}
